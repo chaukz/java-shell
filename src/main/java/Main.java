fc246667d5b9de5fa7f43b427f24f72407508501
chaukz <reboningochauke@gmail.com>
2026-05-25 02:09:07 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
 
