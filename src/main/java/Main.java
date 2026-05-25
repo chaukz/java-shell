b81c14443d6995a2820f95d771f793c0c05cd99f
chaukz <reboningochauke@gmail.com>
2026-05-25 02:27:53 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,12 @@ public class Main {
 
             if (input.equals("exit")) {
                 break;
-            }else if(input.startsWith("echo ")) {
+            }
+            else if(input.startsWith("echo")) {
                 System.out.println(input.substring(5));
             }else {
                 System.out.println(input + ": command not found");
             }
-        
 
     }
 }
